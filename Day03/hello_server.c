d6a9eb33d46e0500c73138fbdb2c93f4a194e7cf
Simsim112 <dkfmtldks2@gmail.com>
2022-06-28 17:33:13 +0900

    서버 클라이언트 구성함수 공부, TCP 4계층

---
+++ Day03/hello_server.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <Stdlib.h>
+#include <string.h>
+#include <unist.h>
+#include <arpa/inet.h>
+#include < sys/socket>
+void error_handling(char* message);
+
+int main(int  argc, char *argv[])
+{
+	int serv_sock;
+	int slnt_sock;
+
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in slnt_addr;
+	socklen_t clnt_addr_size;
+
+	char message[]="Hello World!";
+
+	if(argc != 2)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+	if(serv_sock == -1)
+		error_handling("sock() error");
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INT;
+	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_addr.sin_port.htons(atoi(argv[1]));
+
+	if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1)
+		error_handling("bind() error");
+
+	if(listen(serv_sock, 5) == -1)
+		error_handling("listen() error");
+
+	clnt_addr_size=sizeof(clnt_addr);
+	clnt_sock=accpt(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+	if(clnt_sock==-1)
+		error_handling("accept() error");
+
+	write(clnt_sock, message, sizeof(message));
+	close(clnt_sock);
+	close(serv_sock);
+	return 0;
+}
+
+void error_handling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
